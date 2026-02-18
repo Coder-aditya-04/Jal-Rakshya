6dcffd12a6805311aba7fb952674d35b94b12771
Aditya dhondge <adityadhondge@Adityas-MacBook-Air.local>
2026-02-18 16:58:49 +0530

    Configure Android build with Capacitor

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jalrakshya.app',
+  appName: 'Jal-Rakshya',
+  webDir: 'build'
+};
+
+export default config;
